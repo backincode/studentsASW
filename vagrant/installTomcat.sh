7ce1ed5bceae5141b1d5bc397b0b88c361d95689
marco cappello <kappa89@gmail.com>
2016-05-05 18:34:36 +0200

    update vagrantfile

---
+++ vagrant/installTomcat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# download and install tomcatsudo apt-get update
+echo install tomcat7
+echo ______________________________________________
+sudo apt-get update && sudo apt-get -yf install tomcat7 && install -yf tomcat7-admin
