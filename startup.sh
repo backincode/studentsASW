dd5c543e361c84b31b2ffe46dbe07e6b6d1c1907
Marco Sbaffoni <maffo.anima87@gmail.com>
2016-06-10 23:05:10 +0200

    Cleaning startup.sh

--- startup.sh
+++ startup.sh
@@ -15,6 +15,11 @@ docker build -t studentserver --file=./docker/tomcat/Dockerfile .
 
 ################## STARTUP ################
 
+## cleaning any old version
+docker rm portale portaledb
+
+## starting containers in detach mode
+## N.B. start of portaledb may take longer than expected, check docker logs portaledb.
 docker run --name portaledb -d -p 5432:5432 studentdb 
 sleep 1
 docker run --name portale -d -p 8080:8080 -v /tmp/webapp:/usr/local/tomcat/webapps/ --link portaledb:db studentserver
